3a14f8e651a512f6a1f973ad0474b05c1b54d98d
Guilherme <guicomercial328@gmail.com>
2023-05-14 00:53:27 -0300

    Layout and script adjustments

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
 // alert("Olá como você está? \n Seja BEM-VINDO(A)")
 
-// /*Criando um array de images*/
+/*Criando um array de images*/
 
-// var images=[
-//     'img/baixados.jpeg',
-//     'img/OIP (1).jpeg',
-//     'img/OIP (2).jpeg',
-//     'img/OIP (3).jpeg',
-//     'img/OIP (4).jpeg',
-//     'img/OIP.jpeg',
-// ];
+var images=[
+    'img/baixados.jpeg',
+    'img/OIP (1).jpeg',
+    'img/OIP (2).jpeg',
+    'img/OIP (3).jpeg',
+    'img/OIP (4).jpeg',
+    'img/OIP.jpeg',
+];
 
-// /*declarando variaveis*/
+/*declarando variaveis*/
 
-// let index = 0; /*Posição*/
-// let time = 2000;/*tempo*/
+let index = 0; /*Posição*/
+let time = 2000;/*tempo*/
 
-// /*Criando uma função*/
+/*Criando uma função*/
 
-// function slideshow(){
-//     /*Pega o ID e o caminho da imagem*/
-//     document.getElementById('image').src=images[index]
-//     index++; /*Incremento*/
-//     if(index ==images.length){index=0}
-//     setTimeout("slideshow()",time);
-// }
+function slideshow(){
+    /*Pega o ID e o caminho da imagem*/
+    document.getElementById('image').src=images[index]
+    index++; /*Incremento*/
+    if(index ==images.length){index=0}
+    setTimeout("slideshow()",time);
+}
 
-// slideshow();
+slideshow();
 
 
 
